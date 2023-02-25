fef4bb80ce5b5a03e1109b49f7a394809614a5e1
Saurav Singh <singh.saurav@icloud.com>
2023-02-25 16:11:34 +0530

    updated pom

--- src/test/java/TestRunner/AddCommentTest.java
+++ src/test/java/TestRunner/AddCommentTest.java
@@ -5,5 +5,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src/test/java/features/addComment.feature", plugin = { "pretty",
 		"json:target/jsonReports/cucumber-report.json" }, glue = { "stepDefinitions" })
+
 public class AddCommentTest extends AbstractTestNGCucumberTests {
 }
