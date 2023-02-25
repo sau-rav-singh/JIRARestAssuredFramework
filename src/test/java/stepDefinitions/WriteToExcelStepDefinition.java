3ef35c619562848b51be5d282032b10dc3ba53db
Saurav Singh <singh.saurav@icloud.com>
2023-02-25 11:47:35 +0530

    modified write data step name

--- src/test/java/stepDefinitions/WriteToExcelStepDefinition.java
+++ src/test/java/stepDefinitions/WriteToExcelStepDefinition.java
@@ -13,7 +13,7 @@ public class WriteToExcelStepDefinition {
 	private ExcelSheetReader excelSheetReader;
 	private ExcelSheetWriter excelSheetWriter;
 	
-	@Given("A Workbook named {string} and sheetname as{string} and Row number as {int} is read")
+	@Given("A Workbook named {string} and sheetname as{string} and Row number as {int} is read and to write Data")
     public void a_workbook_with_name_and_sub_sheet_name_and_row_number_is_read(String workbookName, String subSheetName, int rowNumber) throws Exception {
 		System.out.println("rowNumber from feature file is "+rowNumber);
 		excelSheetReader = new ExcelSheetReader(workbookName, subSheetName, rowNumber);
@@ -28,6 +28,7 @@ public class WriteToExcelStepDefinition {
 		if (excelSheetReader == null) {
 			throw new IllegalStateException("ExcelSheetReader is not initialized for the current thread.");
 		}
+		
 		symbol = excelSheetReader.readCell(symbol);
 		orderType = excelSheetReader.readCell(orderType);
 		System.out.println("Symbol is " + symbol);
